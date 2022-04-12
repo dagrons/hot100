2fce1595563fd74f769749434e0579c52f4d6477
heyuehui <heyuehui@bytedance.com>
2022-04-12 10:29:59 +0800

    update

--- i32/app.go
+++ i32/app.go
@@ -1,5 +1,40 @@
 package i32
 
-func longestValidParentheses(s string) int {
-	return 0
+/*
+	最长有效括号，优先考虑dp
+	f[i]: 以下标i结束的最长有效括号的长度
+	没法进行空间优化
+*/
+
+func LongestValidParentheses(s string) int {
+	f := make([]int, len(s))
+	cnt := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			continue
+		}
+		if i-1 >= 0 && s[i-1] == '(' {
+			if i-2 >= 0 {
+				f[i] = f[i-2] + 2
+			} else {
+				f[i] = 2
+			}
+		} else if i-1 >= 0 && i-f[i-1]-1 >= 0 && s[i-f[i-1]-1] == '(' {
+			if i-f[i-1]-2 < 0 {
+				f[i] = f[i-1] + 2
+			} else {
+				f[i] = f[i-1] + 2 + f[i-f[i-1]-2]
+			}
+		}
+		cnt = max(cnt, f[i])
+	}
+	return cnt
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
--- i32/app_test.go
+++ i32/app_test.go
@@ -1,7 +1,20 @@
 package i32_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/dagrons/hot100/i32"
+)
 
 func TestLongestValidParentheses(t *testing.T) {
-
+	tries := map[string]int{
+		"(()":    2,
+		")()())": 4,
+	}
+	for k, v := range tries {
+		ans := i32.LongestValidParentheses(k)
+		if ans != v {
+			t.Errorf("in = %s, out=%d", k, v)
+		}
+	}
 }
